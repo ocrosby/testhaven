5b332b70830b509ccaccdcdb36bea919c1364d7d
Omar Crosby <omar.crosby@gmail.com>
2024-06-08 09:14:38 -0400

    ci: update eslint config

--- packages/test-service/eslint.config.mjs
+++ packages/test-service/eslint.config.mjs
@@ -1,6 +1,19 @@
 import config from "eslint-config-standard";
 
-
-export default [
-  ...[].concat(config),
-];
+export default {
+  languageOptions: {
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+  },
+  rules: {
+    // your rules here
+  },
+  overrides: [
+    {
+      files: ["*.js", "*.mjs"],
+      ...config,
+    },
+  ],
+};
